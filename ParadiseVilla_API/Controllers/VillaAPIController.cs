cb2bf4a117a3a18adcb60aad77922ee2c1526508
quangphu <trancongquangphu10@gmail.com>
2024-06-19 22:40:17 +0700

    First API Endpoint

---
+++ ParadiseVilla_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ParadiseVilla_API.Models;
+
+namespace ParadiseVilla_API.Controllers
+{
+    [Route("/api/[controller]")]
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> GetVillas()
+        {
+            return new List<Villa>
+            {
+                new Villa { Id= 1, Name="Pool View"},
+                new Villa { Id= 2, Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ ParadiseVilla_API/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace ParadiseVilla_API.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
