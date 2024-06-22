9fcfd471daeda87b0ffd14ec4eb0bae04e35c870
quangphu <trancongquangphu10@gmail.com>
2024-06-22 07:42:25 +0700

    Status code in Endpoints

--- ParadiseVilla_API/Controllers/VillaAPIController.cs
+++ ParadiseVilla_API/Controllers/VillaAPIController.cs
@@ -10,14 +10,22 @@ namespace ParadiseVilla_API.Controllers
     public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
             return VillaStore.villaList;
         }
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if(id == 0)
+            {
+                return BadRequest();
+            }
+            var villaStore = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+            if (villaStore == null)
+                return NotFound();
+            else 
+                return Ok(villaStore);
         }
     }
 }
