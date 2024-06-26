28357ef7ea6012fe5c3568a9dc86706dbaa3f83e
quangphu <trancongquangphu10@gmail.com>
2024-06-26 23:31:10 +0700

    Villa Number Model

---
+++ ParadiseVilla_API/Models/DTO/VillaNumberDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParadiseVilla_API.Models.DTO
+{
+    public class VillaNumberDTO
+    {
+        [Required]
+        public int VillaNo { get; set; }
+        public string SpeacialDetails { get; set; }
+    }
+}
---
+++ ParadiseVilla_API/Models/VillaNumber.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ParadiseVilla_API.Models
+{
+    public class VillaNumberDTO
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int VillaNo { get; set; }
+        public string SpeacialDetails { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set;}
+    }
+}
