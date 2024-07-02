2611546bff4ab035f1e8255949e0ecbed0abb19e
quangphu <trancongquangphu10@gmail.com>
2024-07-02 10:22:16 +0700

    Base Service

---
+++ ParadiseVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using ParadiseVilla_Web.Models;
+
+namespace ParadiseVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse reponseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
