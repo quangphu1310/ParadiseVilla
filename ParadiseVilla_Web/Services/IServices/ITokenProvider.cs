b1deec5361b1be805360c6cf94714c3d2bbea613
quangphu <trancongquangphu10@gmail.com>
2024-07-31 13:46:38 +0700

    Create ITokenProvider

---
+++ ParadiseVilla_Web/Services/IServices/ITokenProvider.cs
@@ -0,0 +1,11 @@
+﻿using ParadiseVilla_Web.Models.DTO;
+
+namespace ParadiseVilla_Web.Services.IServices
+{
+    public interface ITokenProvider
+    {
+        void SetToken(TokenDTO tokenDTO);
+        TokenDTO? GetToken();
+        void ClearToken();
+    }
+}
