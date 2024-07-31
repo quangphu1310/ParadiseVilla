086da58c1acc29534047265cc7db04140dbbe651
quangphu <trancongquangphu10@gmail.com>
2024-07-31 09:44:43 +0700

    Create Villa with Image in Web Project

--- ParadiseVilla_Web/Models/DTO/VillaDTO.cs
+++ ParadiseVilla_Web/Models/DTO/VillaDTO.cs
@@ -15,7 +15,6 @@ namespace ParadiseVilla_Web.Models.DTO
         public int Sqft { get; set; }
         public string? ImageUrl { get; set; }
         public string? ImageLocalPath { get; set; }
-        public IFormFile? Image {  get; set; }
         public string Amenity { get; set; }
     }
 }
