82508177743b71f1fce20596b4d9bbbc206e7f4d
quangphu <trancongquangphu10@gmail.com>
2024-07-31 14:05:45 +0700

    Implement Token Provider

--- ParadiseVilla_Web/Services/TokenProvider.cs
+++ ParadiseVilla_Web/Services/TokenProvider.cs
@@ -1,23 +1,43 @@
-﻿using ParadiseVilla_Web.Models.DTO;
+﻿using ParadiseVilla_Utility;
+using ParadiseVilla_Web.Models.DTO;
 using ParadiseVilla_Web.Services.IServices;
 
 namespace ParadiseVilla_Web.Services
 {
     public class TokenProvider : ITokenProvider
     {
+        private readonly IHttpContextAccessor _contextAccessor;
+
+        public TokenProvider(IHttpContextAccessor contextAccessor)
+        {
+            _contextAccessor = contextAccessor; 
+        }
         public void ClearToken()
         {
-            throw new NotImplementedException();
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
         }
 
         public TokenDTO GetToken()
         {
-            throw new NotImplementedException();
+            try
+            {   
+                bool hasAccessToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+                TokenDTO tokenDTO = new TokenDTO()
+                {
+                    Token = accessToken
+                };
+                return hasAccessToken ? tokenDTO : null;
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
         }
 
         public void SetToken(TokenDTO tokenDTO)
         {
-            throw new NotImplementedException();
+            var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken, tokenDTO.Token, cookieOptions);
         }
     }
 }
