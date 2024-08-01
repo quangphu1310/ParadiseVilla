fc63483192c7f1685440c9dd9c82d4e2a6071e1b
quangphu <trancongquangphu10@gmail.com>
2024-08-01 08:47:02 +0700

    Add RefreshToken to Token Provider

--- ParadiseVilla_Web/Services/TokenProvider.cs
+++ ParadiseVilla_Web/Services/TokenProvider.cs
@@ -15,6 +15,7 @@ namespace ParadiseVilla_Web.Services
         public void ClearToken()
         {
             _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.AccessToken);
+            _contextAccessor.HttpContext?.Response.Cookies.Delete(SD.RefreshToken);
         }
 
         public TokenDTO GetToken()
@@ -22,9 +23,11 @@ namespace ParadiseVilla_Web.Services
             try
             {   
                 bool hasAccessToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.AccessToken, out string accessToken);
+                bool hasRefreshToken = _contextAccessor.HttpContext.Request.Cookies.TryGetValue(SD.RefreshToken, out string refreshToken);
                 TokenDTO tokenDTO = new TokenDTO()
                 {
                     AccessToken = accessToken,
+                    RefreshToken = refreshToken
                 };
                 return hasAccessToken ? tokenDTO : null;
             }
@@ -38,6 +41,7 @@ namespace ParadiseVilla_Web.Services
         {
             var cookieOptions = new CookieOptions { Expires = DateTime.UtcNow.AddDays(60) };
             _contextAccessor.HttpContext?.Response.Cookies.Append(SD.AccessToken, tokenDTO.AccessToken, cookieOptions);
+            _contextAccessor.HttpContext?.Response.Cookies.Append(SD.RefreshToken, tokenDTO.RefreshToken, cookieOptions);
         }
     }
 }
